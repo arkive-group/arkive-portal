3472b878823adfed4f0f18a922d0303f8a8a13c6
cupid-dust <ahsanmansoor7572@gmail.com>
2024-10-12 12:26:06 +0500

    accounts integration is complete

--- src/auth/auth-provider.js
+++ src/auth/auth-provider.js
@@ -1,14 +1,14 @@
-'use client'
+"use client";
 
-import PropTypes from 'prop-types'
-import { useEffect, useReducer, useCallback, useMemo, useState } from 'react'
+import PropTypes from "prop-types";
+import { useEffect, useReducer, useCallback, useMemo, useState } from "react";
 import {
   signOut,
   onAuthStateChanged,
   signInWithEmailAndPassword,
   sendSignInLinkToEmail,
   signInWithEmailLink,
-} from 'firebase/auth'
+} from "firebase/auth";
 import {
   collection,
   doc,
@@ -16,128 +16,128 @@ import {
   query,
   getDocs,
   where,
-} from 'firebase/firestore'
+} from "firebase/firestore";
 
 // config
-import { AUTH, DB, STORAGE } from '@/utils/firebase-config'
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
+import { AUTH, DB, STORAGE } from "@/utils/firebase-config";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-import { AuthContext } from './auth-context'
+import { AuthContext } from "./auth-context";
 
 // routes
-import { useRouter } from '@/routes/hooks'
-import { useSnackbar } from '@/components/snackbar'
-import { paths } from '@/routes/paths'
+import { useRouter } from "@/routes/hooks";
+import { useSnackbar } from "@/components/snackbar";
+import { paths } from "@/routes/paths";
 
 // ----------------------------------------------------------------------
 
 const initialState = {
   user: null,
   loading: true,
-}
+};
 
 const reducer = (state, action) => {
-  if (action.type === 'INITIAL') {
+  if (action.type === "INITIAL") {
     return {
       loading: false,
       user: action.payload.user,
-    }
+    };
   }
-  return state
-}
+  return state;
+};
 
 // ----------------------------------------------------------------------
 
 export function AuthProvider({ children }) {
-  const router = useRouter()
-  const { enqueueSnackbar } = useSnackbar()
+  const router = useRouter();
+  const { enqueueSnackbar } = useSnackbar();
 
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const [foundUser, setFoundUser] = useState(null)
-  const findUserByEmail = async email => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [foundUser, setFoundUser] = useState(null);
+  const findUserByEmail = async (email) => {
     try {
-      const usersCollection = collection(DB, 'users')
-      const q = query(usersCollection, where('email', '==', email))
-      const querySnapshot = await getDocs(q)
+      const usersCollection = collection(DB, "users");
+      const q = query(usersCollection, where("email", "==", email));
+      const querySnapshot = await getDocs(q);
 
       if (!querySnapshot.empty) {
-        const userDoc = querySnapshot.docs[0]
-        return { id: userDoc.id, ...userDoc.data() }
+        const userDoc = querySnapshot.docs[0];
+        return { id: userDoc.id, ...userDoc.data() };
       } else {
-        return null
+        return null;
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
   const initialize = useCallback(() => {
     try {
-      onAuthStateChanged(AUTH, async user => {
+      onAuthStateChanged(AUTH, async (user) => {
         if (user) {
-          const userData = foundUser || (await findUserByEmail(user.email))
+          const userData = foundUser || (await findUserByEmail(user.email));
           if (userData) {
             dispatch({
-              type: 'INITIAL',
+              type: "INITIAL",
               payload: {
                 user: userData,
               },
-            })
-            return
+            });
+            return;
           }
         } else {
           dispatch({
-            type: 'INITIAL',
+            type: "INITIAL",
             payload: {
               user: null,
             },
-          })
+          });
         }
-      })
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    initialize()
-  }, [initialize])
+    initialize();
+  }, [initialize]);
 
   // LOGIN WITH EMAIL AND PASSWORD
 
-const actionCodeSettings = {
+  const actionCodeSettings = {
     url: `${
-      process.env.NODE_ENV === 'development'
-        ? 'http://localhost:3000'
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
         : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
     }/auth/finish-login`,
     handleCodeInApp: true,
-  }
+  };
 
   const login = useCallback(async (email, password) => {
     const useCredentials = await signInWithEmailAndPassword(
       AUTH,
       email,
-      password,
-    )
-    return useCredentials
-  }, [])
+      password
+    );
+    return useCredentials;
+  }, []);
 
-  const signup = useCallback(async data => {
-    const existingUser = await findUserByEmail(data.email)
+  const signup = useCallback(async (data) => {
+    const existingUser = await findUserByEmail(data.email);
     if (existingUser) {
       enqueueSnackbar(
-        'User with this email already exists. Please login or use a different email.',
-      )
-      return null
+        "User with this email already exists. Please login or use a different email."
+      );
+      return null;
     }
-    const usersCollection = collection(DB, 'users')
+    const usersCollection = collection(DB, "users");
     try {
       const avatarRef = ref(
         STORAGE,
-        `avatars/${data.avatar.name}_${data.email}`,
-      )
-      await uploadBytes(avatarRef, data.avatar)
-      const avatarUrl = await getDownloadURL(avatarRef)
+        `avatars/${data.avatar.name}_${data.email}`
+      );
+      await uploadBytes(avatarRef, data.avatar);
+      const avatarUrl = await getDownloadURL(avatarRef);
       const user = {
         email: data.email,
         first_name: data.first_name,
@@ -146,82 +146,96 @@ const actionCodeSettings = {
         company: data.company,
         phoneNumber: data.phoneNumber,
         avatar: avatarUrl,
-      }
-      await setDoc(doc(usersCollection), user)
+      };
+      await setDoc(doc(usersCollection), user);
 
-      await fetch('/api/email', {
-        method: 'POST',
+      await fetch("/api/email", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ name: data.first_name, email: data.email }),
-      })
+      });
 
-      await sendSignInLinkToEmail(AUTH, user.email, actionCodeSettings)
-      router.push(paths.auth.verify + `?email=${user.email}`)
-      return user
+      await sendSignInLinkToEmail(AUTH, user.email, actionCodeSettings);
+      router.push(paths.auth.verify + `?email=${user.email}`);
+      return user;
     } catch (error) {
-      console.error('Error creating user:', error)
-      return null
+      console.error("Error creating user:", error);
+      return null;
     }
-  }, [])
+  }, []);
 
-  const loginWithLink = useCallback(async email => {
+  const updateUser = useCallback(async (data) => {
+    // Update the local context
+    dispatch({
+      type: "INITIAL",
+      payload: {
+        user: {
+          ...state.user,
+          ...data,
+        },
+      },
+    });
+  }, []);
+
+  const loginWithLink = useCallback(async (email) => {
     try {
-      const userData = await findUserByEmail(email)
+      const userData = await findUserByEmail(email);
       if (userData) {
-        setFoundUser(userData)
-        await sendSignInLinkToEmail(AUTH, email, actionCodeSettings)
-        router.push(paths.auth.verify + `?email=${email}`)
+        setFoundUser(userData);
+        await sendSignInLinkToEmail(AUTH, email, actionCodeSettings);
+        router.push(paths.auth.verify + `?email=${email}`);
       } else {
-        router.push(paths.auth.register + `?email=${email}`)
+        router.push(paths.auth.register + `?email=${email}`);
       }
     } catch (error) {
-      console.error('Error sending email link:', error)
+      console.error("Error sending email link:", error);
     }
-  }, [])
+  }, []);
 
-  const checkLoginLink = useCallback(async email => {
+  const checkLoginLink = useCallback(async (email) => {
     try {
-      const credentials = await signInWithEmailLink(AUTH, email)
+      const credentials = await signInWithEmailLink(AUTH, email);
       if (credentials.user.emailVerified) {
-        state.user = foundUser
-        router.push(paths.home)
-        setFoundUser(null)
+        state.user = foundUser;
+        router.push(paths.home);
+        setFoundUser(null);
       }
-      return credentials
+      return credentials;
     } catch (error) {
-      console.error('Error signing in:', error)
-      return null
+      console.error("Error signing in:", error);
+      return null;
     }
-  }, [])
+  }, []);
 
   // LOGOUT
   const logout = useCallback(async () => {
-    await signOut(AUTH)
-    state.user = null
-    router.push(paths.auth.login)
-  }, [])
+    await signOut(AUTH);
+    state.user = null;
+    router.push(paths.auth.login);
+  }, []);
 
   // ----------------------------------------------------------------------
 
-  const checkAuthenticated = state.user ? 'authenticated' : 'unauthenticated'
+  const checkAuthenticated = state.user ? "authenticated" : "unauthenticated";
 
-  const status = state.loading ? 'loading' : checkAuthenticated
+  const status = state.loading ? "loading" : checkAuthenticated;
 
   const memoizedValue = useMemo(
     () => ({
       user: state.user,
-      method: 'firebase',
-      loading: status === 'loading',
-      authenticated: status === 'authenticated',
-      unauthenticated: status === 'unauthenticated',
+      method: "firebase",
+      loading: status === "loading",
+      authenticated: status === "authenticated",
+      unauthenticated: status === "unauthenticated",
       //
       login,
       logout,
       signup,
       loginWithLink,
       checkLoginLink,
+      updateUser,
     }),
     [
       status,
@@ -232,16 +246,17 @@ const actionCodeSettings = {
       signup,
       loginWithLink,
       checkLoginLink,
-    ],
-  )
+      updateUser,
+    ]
+  );
 
   return (
     <AuthContext.Provider value={memoizedValue}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 AuthProvider.propTypes = {
   children: PropTypes.node,
-}
+};
--- src/sections/account/account-details.js
+++ src/sections/account/account-details.js
@@ -1,211 +1,263 @@
 "use client";
 import {
-  Avatar,
   Box,
-  Button,
   Card,
   CardActions,
   CardContent,
   CardHeader,
   Divider,
   Grid,
-  Link,
-  Switch,
-  TextField,
   Typography,
 } from "@mui/material";
+import { useSnackbar } from "src/components/snackbar";
+import FormProvider, {
+  RHFTextField,
+  RHFUploadAvatar,
+} from "src/components/hook-form";
+import * as Yup from "yup";
+import { useCallback, useMemo } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuthContext, usePremiumStatus } from "@/auth/hooks";
+// utils
+import { fData } from "src/utils/format-number";
+import LoadingButton from "@mui/lab/LoadingButton";
+import Link from "next/link";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { DB, STORAGE } from "@/utils/firebase-config";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const AccountGeneralSettings = () => {
+  const { user, updateUser } = useAuthContext();
+  const { isPremium } = usePremiumStatus(user);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const userSchema = Yup.object().shape({
+    first_name: Yup.string().required("First name is required"),
+    last_name: Yup.string().required("Last name is required"),
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email address"),
+    phoneNumber: Yup.string().required("Phone number is required"),
+    company: Yup.string().required("Company is required"),
+    role: Yup.string().required("Role is required"),
+    avatar: Yup.mixed().nullable().required("Avatar is required"),
+    country: Yup.string().required("Country is required"),
+    state: Yup.string().required("State/Region is required"),
+    city: Yup.string().required("City is required"),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      first_name: user?.first_name ?? "",
+      last_name: user?.last_name ?? "",
+      role: user?.role ?? "",
+      email: user?.email ?? "",
+      company: user?.company ?? "",
+      avatar: user?.avatar ?? null,
+      phoneNumber: user?.phoneNumber ?? "",
+      country: user?.country ?? "",
+      state: user?.state ?? "",
+      city: user?.city ?? "",
+    }),
+    []
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(userSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    setValue,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = methods;
+
+  const values = watch();
+
+  // const onSubmit = handleSubmit(async (data) => {
+  //   try {
+  //     const user = await signup(data);
+  //     reset();
+  //     enqueueSnackbar(
+  //       user ? "Signup successful! - Please verify email" : "Signup Error...!"
+  //     );
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // });
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      const userId = user?.id; // Assuming you have the user's ID stored in `user`
+
+      if (!userId) {
+        enqueueSnackbar("User ID is missing.", { variant: "error" });
+        return;
+      }
+
+      // Check if avatar is a file and needs to be uploaded
+      if (data.avatar instanceof File) {
+        const avatarRef = ref(
+          STORAGE,
+          `avatars/${data.avatar.name}_${data.email}`
+        );
+
+        // Upload the avatar file to Firebase Storage
+        await uploadBytes(avatarRef, data.avatar);
+
+        // Get the avatar URL after uploading
+        const avatarUrl = await getDownloadURL(avatarRef);
+        data.avatar = avatarUrl;
+      }
+      // If avatar is already a string (URL), just keep it as is
+      // No changes needed in this case
+
+      // Define user data to update in Firestore
+      const userData = {
+        email: data.email,
+        first_name: data.first_name,
+        last_name: data.last_name,
+        role: data.role,
+        company: data.company,
+        phoneNumber: data.phoneNumber,
+        avatar: data.avatar, // Can be URL or newly uploaded file URL
+        country: data.country,
+        state: data.state,
+        city: data.city,
+      };
+
+      const usersCollection = collection(DB, "users");
+      // Get reference to the user's document and update it
+      const userDocRef = doc(usersCollection, userId);
+      await setDoc(userDocRef, userData, { merge: true }); // Merge to avoid overwriting
+      await updateUser({ ...userData, id: user?.id });
+      enqueueSnackbar("User data updated successfully!");
+    } catch (error) {
+      console.error("Error updating user data:", error);
+      enqueueSnackbar("Error updating user data.", { variant: "error" });
+    }
+  });
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue("avatar", newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
   return (
-    <Grid container spacing={3} mt={1}>
-      <Grid item lg={4} md={6} xl={3} xs={12}>
-        <Card>
-          <CardContent>
-            <Box
-              sx={{
-                alignItems: "center",
-                display: "flex",
-                flexDirection: "column",
-                textAlign: "center",
-              }}
-            >
+    <FormProvider methods={methods} onSubmit={onSubmit}>
+      <Grid container spacing={3} mt={1}>
+        <Grid item lg={4} md={6} xl={3} xs={12}>
+          <Card>
+            <CardContent>
               <Box
                 sx={{
-                  p: 1,
-                  border: (theme) => `1px dashed ${theme.palette.divider}`,
-                  borderRadius: "50%",
+                  alignItems: "center",
+                  display: "flex",
+                  flexDirection: "column",
+                  textAlign: "center",
                 }}
               >
-                <Avatar sx={{ height: 100, width: 100 }} />
+                <RHFUploadAvatar
+                  name="avatar"
+                  maxSize={3145728}
+                  onDrop={handleDrop}
+                  helperText={
+                    <Typography
+                      variant="caption"
+                      sx={{
+                        // mt: 3,
+                        mx: "auto",
+                        display: "block",
+                        textAlign: "center",
+                        color: "text.disabled",
+                      }}
+                    >
+                      Allowed *.jpeg, *.jpg, *.png, *.gif
+                      <br /> max size of {fData(3145728)}
+                    </Typography>
+                  }
+                />
               </Box>
+            </CardContent>
+            <CardActions sx={{ justifyContent: "center" }}>
               <Typography
-                color="textPrimary"
-                sx={{ mt: 1 }}
-                variant="subtitle2"
+                color="textSecondary"
+                variant="body2"
+                textAlign="center"
               >
-                User Name
-              </Typography>
-              <Typography color="textSecondary" variant="body2">
                 Your plan:
-                <Link color="primary" to="/dashboard/account">
-                  Plan Name
+                <Link
+                  href={isPremium ? "/settings/payouts" : "/settings/billing"}
+                >
+                  {isPremium ? "Eco Master Plan" : "Eco Starter Plan"}
                 </Link>
               </Typography>
-            </Box>
-          </CardContent>
-          <CardActions>
-            <Button color="primary" fullWidth variant="text">
-              Remove Picture
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item lg={8} md={6} xl={9} xs={12}>
-        <form>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item lg={8} md={6} xl={9} xs={12}>
           <Card>
             <CardHeader title="Basic Details" />
             <CardContent>
               <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Name"
-                    variant="outlined"
-                    size="small"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Email Address"
-                    type="email"
-                    variant="outlined"
-                    size="small"
-                  />
+                <Grid item md={6} xs={12}>
+                  <RHFTextField name="first_name" label="First Name" />
                 </Grid>
                 <Grid item md={6} xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Phone Number"
-                    variant="outlined"
-                    size="small"
-                  />
+                  <RHFTextField name="last_name" label="Last Name" />
                 </Grid>
                 <Grid item md={6} xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Country"
-                    variant="outlined"
-                    size="small"
-                  />
+                  <RHFTextField name="email" label="Email Address" disabled />
                 </Grid>
                 <Grid item md={6} xs={12}>
-                  <TextField
-                    fullWidth
-                    label="State/Region"
-                    variant="outlined"
-                    size="small"
-                  />
+                  <RHFTextField name="phoneNumber" label="Phone Number" />
                 </Grid>
                 <Grid item md={6} xs={12}>
-                  <TextField
-                    fullWidth
-                    label="City"
-                    variant="outlined"
-                    size="small"
-                  />
+                  <RHFTextField name="company" label="Company" />
                 </Grid>
-
+                <Grid item md={6} xs={12}>
+                  <RHFTextField name="role" label="Role" disabled />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <RHFTextField name="country" label="Country" />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <RHFTextField name="state" label="State/Region" />
+                </Grid>
                 <Grid item xs={12}>
-                  <TextField
-                    fullWidth
-                    label="Biography (optional)"
-                    type="text"
-                    variant="outlined"
-                    size="small"
-                    multiline
-                    minRows={3}
-                  />
-                </Grid>
-                <CardHeader title="Billing Details" />
-                <CardContent>
-                  <Grid container spacing={3}>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        fullWidth
-                        label="Address"
-                        variant="outlined"
-                        size="small"
-                      />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        fullWidth
-                        label="City"
-                        variant="outlined"
-                        size="small"
-                      />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        fullWidth
-                        label="State/Region"
-                        variant="outlined"
-                        size="small"
-                      />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        fullWidth
-                        label="Zip Code"
-                        variant="outlined"
-                        size="small"
-                      />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        fullWidth
-                        label="Tax ID"
-                        variant="outlined"
-                        size="small"
-                      />
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-                      <TextField
-                        fullWidth
-                        label="Currency"
-                        variant="outlined"
-                        size="small"
-                      />
-                    </Grid>
-                  </Grid>
-                </CardContent>
-                <Grid item xs={12}>
-                  <Typography
-                    color="textPrimary"
-                    gutterBottom
-                    variant="subtitle2"
-                  >
-                    Public Profile
-                  </Typography>
-                  <Typography color="textSecondary" variant="body2">
-                    Means that anyone viewing your profile will be able to see
-                    your contact details
-                  </Typography>
-                  <Switch color="primary" />
+                  <RHFTextField name="city" label="City" />
                 </Grid>
               </Grid>
             </CardContent>
             <Divider />
             <Box sx={{ display: "flex", justifyContent: "flex-end", p: 2 }}>
-              <Button color="primary" type="submit" variant="contained">
+              <LoadingButton
+                type="submit"
+                color="primary"
+                variant="contained"
+                loading={isSubmitting}
+              >
                 Save Changes
-              </Button>
+              </LoadingButton>
             </Box>
           </Card>
-        </form>
+        </Grid>
       </Grid>
-    </Grid>
+    </FormProvider>
   );
 };
 
