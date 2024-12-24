5cc7da01da5da900b7b24bab3016838bf3bc4a48
arkivegroup <service@arkive.nl>
2024-12-23 20:49:43 -0500

    bugfix: fix phone schema (#46)
    Co-authored-by: EdenWuyifan <yfw215@nyu.edu>

--- src/components/hook-form/rhf-text-field.js
+++ src/components/hook-form/rhf-text-field.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { useFormContext, Controller } from 'react-hook-form';
 // @mui
 import TextField from '@mui/material/TextField';
+import MuiPhoneNumber from 'mui-phone-number';
 
 // ----------------------------------------------------------------------
 
@@ -13,22 +14,36 @@ export default function RHFTextField({ name, helperText, type, ...other }) {
       name={name}
       control={control}
       render={({ field, fieldState: { error } }) => (
-        <TextField
-          {...field}
-          fullWidth
-          type={type}
-          value={type === 'number' && field.value === 0 ? '' : field.value}
-          onChange={(event) => {
-            if (type === 'number') {
-              field.onChange(Number(event.target.value));
-            } else {
-              field.onChange(event.target.value);
-            }
-          }}
-          error={!!error}
-          helperText={error ? error?.message : helperText}
-          {...other}
-        />
+        name === 'phoneNumber' ? (
+          <MuiPhoneNumber
+            {...field}
+            fullWidth
+            defaultCountry={'nl'}
+            onChange={(value) => {
+              field.onChange(value);
+            }}
+            error={!!error}
+            helperText={error ? error?.message : helperText}
+            {...other}
+          />
+        ) : (
+          <TextField
+            {...field}
+            fullWidth
+            type={type}
+            value={type === 'number' && field.value === 0 ? '' : field.value}
+            onChange={(event) => {
+              if (type === 'number') {
+                field.onChange(Number(event.target.value));
+              } else {
+                field.onChange(event.target.value);
+              }
+            }}
+            error={!!error}
+            helperText={error ? error?.message : helperText}
+            {...other}
+          />
+        )
       )}
     />
   );
