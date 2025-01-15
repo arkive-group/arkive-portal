c5b33e369d1d436c08548d6f26a47ac4820a976e
Daria Epaneshnikova <daria@piggy.nl>
2025-01-15 19:19:16 +0100

    add more icons and add some conditional logic to separate channels coming from api into separate ones for the view - needs improvements in the future and refactor

--- src/sections/home/active-sales-channels-sidebar.tsx
+++ src/sections/home/active-sales-channels-sidebar.tsx
@@ -11,11 +11,14 @@ import {
   Icon,
 } from "@mui/material";
 
-
 import MailIcon from "./icons/MailIcon";
 import LinktreeIcon from "./icons/LinktreeIcon";
 import ShopIcon from "./icons/ShopIcon";
 import TiktokIcon from "@/assets/icons/TiktokIcon";
+import FacebookIcon from "./icons/FacebookIcon";
+import InstagramIcon from "./icons/InstagramIcon";
+import YoutubeIcon from "./icons/YoutubeIcon";
+import GoogleMarketplaceUrl from "../../components/active-channel/icons/google-marketplace.svg";
 
 export function ActiveSalesChannelsSidebar({ activeChannels }) {
   const activeChannelsIcons = {
@@ -23,8 +26,11 @@ export function ActiveSalesChannelsSidebar({ activeChannels }) {
     Inbox: <MailIcon />,
     Linktree: <LinktreeIcon />,
     Shop: <ShopIcon />,
+    Facebook: "",
+    Instagram: "",
+    Google: "",
+    Youtube: "",
     "Online Store": null,
-    "Facebook & Instagram": null,
   };
 
   return (
@@ -42,29 +48,128 @@ export function ActiveSalesChannelsSidebar({ activeChannels }) {
           {Object.keys(activeChannels).map((channel) => {
             const name = activeChannels[channel].name;
             const icon = activeChannelsIcons[name] || "";
-            return (
-              <ListItem key={channel} style={{ textAlign: "left" }}>
-                <ListItemIcon sx={{ marginRight: 1 }}>
-                  <Icon
-                    sx={{
-                      width: "2rem",
-                      height: "2rem",
-                      background: "#efefef",
-                      borderRadius: "100%",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      fontSize: "16px",
-                    }}
-                  >
-                    {icon}
-                  </Icon>
-                </ListItemIcon>
-                <Typography style={{ opacity: 0.75 }} fontSize={"0.875rem"}>
-                  {activeChannels[channel].name}
-                </Typography>
-              </ListItem>
-            );
+
+            if (name === "Facebook & Instagram") {
+              return (
+                <>
+                  <ListItem key={channel} style={{ textAlign: "left" }}>
+                    <ListItemIcon sx={{ marginRight: 1 }}>
+                      <Icon
+                        sx={{
+                          width: "2rem",
+                          height: "2rem",
+                          background: "#efefef",
+                          borderRadius: "100%",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          fontSize: "16px",
+                        }}
+                      >
+                        <FacebookIcon />
+                      </Icon>
+                    </ListItemIcon>
+                    <Typography style={{ opacity: 0.75 }} fontSize={"0.875rem"}>
+                      {"Facebook"}
+                    </Typography>
+                  </ListItem>
+                  <ListItem key={channel} style={{ textAlign: "left" }}>
+                    <ListItemIcon sx={{ marginRight: 1 }}>
+                      <Icon
+                        sx={{
+                          width: "2rem",
+                          height: "2rem",
+                          background: "#efefef",
+                          borderRadius: "100%",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          fontSize: "16px",
+                        }}
+                      >
+                        <InstagramIcon />
+                      </Icon>
+                    </ListItemIcon>
+                    <Typography style={{ opacity: 0.75 }} fontSize={"0.875rem"}>
+                      {"Instagram"}
+                    </Typography>
+                  </ListItem>
+                </>
+              );
+            } else if (name === "Google & YouTube") {
+              return (
+                <>
+                  <ListItem key={channel} style={{ textAlign: "left" }}>
+                    <ListItemIcon sx={{ marginRight: 1 }}>
+                      <Icon
+                        sx={{
+                          width: "2rem",
+                          height: "2rem",
+                          background: "#efefef",
+                          backgroundImage: `url(${GoogleMarketplaceUrl})`,
+                          backgroundSize: "contain",
+                          backgroundPosition: "center",
+                          backgroundRepeat: "no-repeat",
+                          borderRadius: "100%",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          fontSize: "16px",
+                          boxShadow: 1,
+                        }}
+                      />
+                    </ListItemIcon>
+                    <Typography style={{ opacity: 0.75 }} fontSize={"0.875rem"}>
+                      {"Google Marketplace"}
+                    </Typography>
+                  </ListItem>
+                  <ListItem key={channel} style={{ textAlign: "left" }}>
+                    <ListItemIcon sx={{ marginRight: 1 }}>
+                      <Icon
+                        sx={{
+                          width: "2rem",
+                          height: "2rem",
+                          background: "#efefef",
+                          borderRadius: "100%",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          fontSize: "16px",
+                        }}
+                      >
+                        <YoutubeIcon />
+                      </Icon>
+                    </ListItemIcon>
+                    <Typography style={{ opacity: 0.75 }} fontSize={"0.875rem"}>
+                      {"Youtube"}
+                    </Typography>
+                  </ListItem>
+                </>
+              );
+            } else
+              return (
+                <ListItem key={channel} style={{ textAlign: "left" }}>
+                  <ListItemIcon sx={{ marginRight: 1 }}>
+                    <Icon
+                      sx={{
+                        width: "2rem",
+                        height: "2rem",
+                        background: "#efefef",
+                        borderRadius: "100%",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        fontSize: "16px",
+                      }}
+                    >
+                      {icon}
+                    </Icon>
+                  </ListItemIcon>
+                  <Typography style={{ opacity: 0.75 }} fontSize={"0.875rem"}>
+                    {activeChannels[channel].name}
+                  </Typography>
+                </ListItem>
+              );
           })}
         </List>
       </Card>
